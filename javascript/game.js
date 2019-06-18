9403e64a824cf5c6ec7ca017c6e02eb94866c234
ryansims265 <ryansims265@gmail.com>
2019-06-18 18:32:20 -0400

    Created variables and .textcontent for each variable

--- javascript/game.js
+++ javascript/game.js
@@ -1,29 +1,47 @@
-var wins = 0;
-var losses = 0;
-var guessesleft = 9;
-var guessesfar = 0;
+var winsintial = 0;
+var lossesintial = 0;
+var guessesleftintial = 9;
+
+var winsV = document.getElementById("wins");
+var lossesV = document.getElementById("losses");
+var numberV = document.getElementById("number");
+
+
+
+
+document.getElementById("wins").textContent = "Wins: " + winsintial;
+document.getElementById("losses").textContent = "Losses: "+ lossesintial;
+document.getElementById("number").textContent = "Guesses left: " + guessesleftintial;
 
 //First the computer needs to randomly choose a letter from an array and be assigned a variable 
-
-var computerguess = possibleguesses[(Math.random() * 26) | 0];
 var possibleguesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-console.log(computerguess);
-console.log(possibleguesses.length);
+var computerguess = possibleguesses[Math.floor(Math.random() * possibleguesses.length)];
+
+console.log("Computer guess = " + computerguess);
+
+
+
+
+
 
 //Then the user must choose a letter and saved as a variable 
-var userguess = on.onkeydown
-document.onkeyup = function(event) {}
+document.onkeyup = function(event) {
+    var userguess = event.key;
+    console.log("User Guess = " + userguess);
 
 //Then the condition of the is the two guess variables are equal to each other are met
-if (computerguess = userguess){
- wins + 1
- guessesleft = 9
- guessesfar = 0
+if (computerguess === userguess){
+ winsV.textcontent = "Wins " + wins + 1;
+ numberV.textcontent = guessesleftintial;
+ console.log("WINNER");
+ 
 }
 
 else if (computerguess != userguess){
- losses + 1
- guessesleft - 1
- guessesfar + 1
+    winsV.textcontent = "Wins " + wins + 0;
+    numberV.textcontent = guessesleftintial - 1;
+    console.log("LOSER");
+   }
+
 }
 //This then needs to loop and repeat the game but keep track of the number of wins and losses of previous games 
