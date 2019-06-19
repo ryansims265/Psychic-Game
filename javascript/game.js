a941b481f8a06faf212387810a6bdbbb55db1012
ryansims265 <ryansims265@gmail.com>
2019-06-18 23:48:45 -0400

    Created styling for results page

--- javascript/game.js
+++ javascript/game.js
@@ -18,11 +18,11 @@ document.getElementById("number").textContent = "Guesses left: " + guessesleft;
 //First the computer needs to randomly choose a letter from an array and be assigned a variable 
 var possibleguesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var computerguess = possibleguesses[Math.floor(Math.random() * possibleguesses.length)];
 //Loop starting here 
+var computerguess = possibleguesses[Math.floor(Math.random() * possibleguesses.length)];
 
 
-// boolean, while loops 
+// the userguess will error when you first open the console log but that is because you havent guessed yet 
 
 
 
@@ -42,13 +42,13 @@ if (computerguess === userguess){
 
 }
 
-else if (computerguess != userguess){
-    winsV.textcontent = "Wins " + wins + 0;
-    numberV.textcontent = guessesleft - 1;
-    document.getElementById("losses").textContent = "Losses: "+ (numberoflosses + 1);
-    document.getElementById("already").textContent = userguess;
+else if (computerguess !== userguess){
+    winsV.textcontent = "Wins " + (wins + 0);
+    numberV.textcontent = (guessesleft - 1);
+    document.getElementById("already").textContent = "Already Guessed: " + userguess;
     console.log("LOSER");
-    
+    lose();
+    //User guess resets the already guess each time, it needs to list them instead 
    }
 
 }
@@ -64,4 +64,11 @@ document.getElementById("number").textContent = "Guesses already: " + userguess;
       
   }
 
+  function lose (){
+    document.getElementById("losses").textContent = "Losses: "+ (numberoflosses + 1);
+    document.getElementById("number").textContent = "Guesses Left: " + (guessesleft - 1); 
+    var computerguess = possibleguesses[Math.floor(Math.random() * possibleguesses.length)];
+
+  }
 //This then needs to loop and repeat the game but keep track of the number of wins and losses of previous games 
+//This game should loop until you win 10 times or you run out of guesses
